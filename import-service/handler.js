b39d911c891513cbd5263f3bfac2ff4d5a15dab7
‘MeroshiniRaja’ <meroshiniraja19@gmail.com>
2022-10-28 18:34:16 +0530

    importFileParser

--- import-service/handler.js
+++ import-service/handler.js
@@ -27,5 +27,39 @@ module.exports.importProductsFile = async (event) => {
       body: 'Please check the logs'
     }
   }
+};
+
+module.exports.importFileParser = async (event) => {
+
+  for( const record of event.Records){
+
+    //Create read stream
+    const params = {
+      Bucket: BUCKET,
+      Key: record.s3.object.key
+    }
+
+    const s3Stream = s3.getObject(params).createReadStream();
+
+    s3Stream.on('data',(row)=>{
+      console.log(row.toString());
+    }).on('end',()=>{
+      console.log("Reached End!")
+    })
+
+    // Copy object and delete object
+    await s3.copyObject({
+      Bucket: BUCKET,
+      CopySource: BUCKET + '/' + record.s3.object.key,
+      Key: record.s3.object.key.replace('uploaded','parsed')
+    }).promise();
+
+    await s3.deleteObject({
+      Bucket:BUCKET,
+      Key: record.s3.object.key
+    }).promise();
+
+    console.log('Parsed file'+ record.s3.object.key.split('/')[1] +'is created')
+  }
 
 };
