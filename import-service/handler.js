c858de9d5f6bd28ba9496b6370c16645638d5629
‘MeroshiniRaja’ <meroshiniraja19@gmail.com>
2022-11-09 11:43:56 +0530

    chnages for sqs and sns

--- import-service/handler.js
+++ import-service/handler.js
@@ -5,11 +5,23 @@ const S3 = require('aws-sdk/clients/s3');
 const BUCKET = 'aws-import-service-bucket';
 const s3 = new S3();
 const csvParser = require('csv-parser');
+const QueueURL = `https://sqs.us-east-1.amazonaws.com/342406933836/catalogItemsQueue`;
+const AWS = require("aws-sdk");
+const sqs = new AWS.SQS({
+    region: "us-east-1",
+});
+const {v4: uuid} = require('uuid');
+const sns = new AWS.SNS({
+  region: "us-east-1",
+});
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
 
 module.exports.importProductsFile = async (event) => {
   let {name} = event.queryStringParameters;
-  console.log(`parsed/${name}`);
-  let objectKey = `parsed/${name}`;
+  console.log(`uploaded/${name}`);
+  let objectKey = `uploaded/${name}`;
   try{
     let params = {
       Bucket: BUCKET,
@@ -32,38 +44,99 @@ module.exports.importProductsFile = async (event) => {
 };
 
 module.exports.importFileParser = async (event) => {
-
-  for( const record of event.Records){
-
-    //Create read stream
-    const params = {
-      Bucket: BUCKET,
-      Key: record.s3.object.key
+  try {
+    const results = await parseFile(event.Records[0].s3.object.key)
+    console.log(results);
+    for (const result of results) {
+        await sqs.sendMessage({
+            QueueUrl: QueueURL,
+            MessageBody: JSON.stringify(result),
+        }).promise();
     }
-    console.log("Streaming File");
-    const s3Stream = s3.getObject(params).createReadStream();
 
-    s3Stream.pipe(csvParser()).on('data',(row)=>{
-      console.log("Parsed Data", row);
-    }).on('end',()=>{
-      console.log("Reached End!")
-    })
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({message: `All files been processed.`}),
+    };
+  } catch (e) {
+      return {
+          statusCode: 500,
+          headers: {
+              'Access-Control-Allow-Origin': '*',
+          },
+          body: JSON.stringify({message: `ERROR: ${e}`}),
+      };
+  }
+};
 
-    // Copy object and delete object
-    console.log("Copying file");
-    await s3.copyObject({
+function parseFile(fileName) {
+  const s3Params = {
       Bucket: BUCKET,
-      CopySource: BUCKET + '/' + record.s3.object.key,
-      Key: record.s3.object.key.replace('uploaded','parsed')
-    }).promise();
-
-    console.log("Deleting file");
-    await s3.deleteObject({
-      Bucket:BUCKET,
-      Key: record.s3.object.key
-    }).promise();
-
-    console.log('Parsed file'+ record.s3.object.key.split('/')[1] +'is created')
+      Key: fileName,
   }
 
-};
+  return new Promise((resolve) => {
+      let results = [];
+      s3.getObject(s3Params).createReadStream().pipe(csvParser())
+          .on('data', (data) => results.push(data))
+          .on('end', () => {
+              return resolve(results)
+          });
+  });
+}
+
+module.exports.catalogBatchProcess = async (event) => {
+  try {
+    for (const record of event.Records) {
+        console.log('Message Body -->  ', record.body);
+        const item = await JSON.parse(record.body)
+
+        const id = uuid();
+        
+        await documentClient.put({
+          TableName: "ProductsListTable",
+          Item: {
+              id,
+              title: item.title,
+              description: item.description,
+              price: item.price
+          },
+        }).promise();
+        await documentClient.put({
+          TableName: "StockListTable",
+          Item: {
+              product_id: id,
+              count: item.count ?? 1,
+          },
+        }).promise();
+    }
+
+    const snsParams = {
+      TopicArn: 'arn:aws:sns:us-east-1:342406933836:createProductTopic',
+      Subject: 'Products processed.',
+      Message: `Products processed: ${JSON.stringify(event.Records)}`,
+    };
+
+    await sns.publish(snsParams).promise();
+
+    return {
+      statusCode: 200,
+      headers: {
+          'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({message: `All files been processed.`}),
+    };
+  }
+  catch (e) {
+    return {
+        statusCode: 500,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({message: `ERROR: ${e}`}),
+    };
+  }
+}
